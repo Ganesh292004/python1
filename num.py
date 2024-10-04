8ec0f8382f8625ea1f3b452557d4a351ad123f3b
Ganesh292004 <ganeshreddysavanam@gmail.com>
2024-10-04 10:40:10 +0530

    Add files via upload

---
+++ num.py
@@ -0,0 +1,10 @@
+real=float(input("enter real part of the first complex:"))
+image1=float(input("enter real part of the first complex:"))
+real2=float(input("enter real part of the first complex:"))
+image2=float(input("enter real part of the first complex:"))
+complex1=complex(real1.image1)
+complex=complex(real2,image2)
+sum_complex=complex1+complex2
+product_complex=complex1*complex2
+print(f"the sum of the complex numbers is:{sum_complex}")
+print(f"the product of the complex number is:{product_complex}")
